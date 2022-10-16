758e2c9f3b0c552003a394d93789eb5757e8e740
Zaid Neurothrone <54468049+neurothrone@users.noreply.github.com>
2022-10-16 21:28:38 +0200

    Initial Commit

---
+++ BowlingAR/BowlingARApp.swift
@@ -0,0 +1,17 @@
+//
+//  BowlingARApp.swift
+//  BowlingAR
+//
+//  Created by Zaid Neurothrone on 2022-10-16.
+//
+
+import SwiftUI
+
+@main
+struct BowlingARApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
